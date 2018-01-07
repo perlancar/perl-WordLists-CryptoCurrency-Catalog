02365af308af9fe80c6e012d61eaa0faf34f1b51
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-07 08:14:26 +0700

    Initial commit

---
+++ lib/WordLists/CryptoCurrency/Catalog.pm
@@ -0,0 +1,32 @@
+package WordLists::CryptoCurrency::Catalog;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Wordlists from cryptocurrency catalog
+
+=head1 DESCRIPTION
+
+This distribution contains the following modules:
+
+=over
+
+=item * L<WordList::CryptoCurrency::Catalog::Symbol>
+
+=item * L<WordList::CryptoCurrency::Catalog::Name>
+
+=item * L<WordList::CryptoCurrency::Catalog::NameSymbol>
+
+=item * L<WordList::CryptoCurrency::Catalog::Safename>
+
+=back
+
+They are sourced from L<CryptoCurrency::Catalog>.
+
+
+=head1 SEE ALSO
+
+L<CryptoCurrency::Catalog>
+
+=cut
